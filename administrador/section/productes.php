67e098d4f9989330508e54acafc55c45a91e2a0e
joanplanas7 <72151839+joanplanas7@users.noreply.github.com>
2022-04-22 00:46:58 +0200

    funcions dels botons acabades (menys cancelar)

--- administrador/section/productes.php
+++ administrador/section/productes.php
@@ -1,4 +1,4 @@
-<?php include("../template/capçalera.php"); ?>
+<?php require("../template/capçalera.php"); ?>
 
 <?php
 
@@ -20,15 +20,47 @@ switch ($accion) {
         break;
     
     case 'modificar':
-        echo "2";
+        $sentenciaSQL = $conexio->prepare("UPDATE llibres set nom=:nom WHERE id=:id");
+        $sentenciaSQL->bindParam(':nom', $txtNom);
+        $sentenciaSQL->bindParam(':id', $txtID);
+        $sentenciaSQL->execute();
+
+        if ($txtImg != ""){
+            $sentenciaSQL = $conexio->prepare("UPDATE llibres set imatge=:imatge WHERE id=:id");
+            $sentenciaSQL->bindParam(':imatge', $txtImg);
+            $sentenciaSQL->bindParam(':id', $txtID);
+            $sentenciaSQL->execute();
+        }
+
         break;
     
     case 'cancelar': 
         echo "3";
         break;
+
+    case 'Seleccionar': 
+        $sentenciaSQL = $conexio->prepare("SELECT * FROM llibres WHERE id=:id");
+        $sentenciaSQL->bindParam(':id', $txtID);
+        $sentenciaSQL->execute();
+
+        $llibre = $sentenciaSQL->fetch(PDO::FETCH_LAZY);
+
+        $txtNom = $llibre['nom'];
+        $txtImg = $llibre['imatge'];
+        break;
+
+    case 'Eliminar': 
+        $sentenciaSQL = $conexio->prepare("DELETE FROM llibres WHERE id=:id");
+        $sentenciaSQL->bindParam(':id', $txtID);
+        $sentenciaSQL->execute();
+        break;
     
 }
 
+$sentenciaSQL = $conexio->prepare("SELECT * FROM llibres");
+$sentenciaSQL->execute();
+
+$llistaLlibres = $sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 
@@ -42,17 +74,17 @@ switch ($accion) {
             <form method="post" enctype="multipart/form-data">
 
                 <div class = "form-group">
-                    <label for="txtID"> ID: </label>
-                    <input type="text" class="form-control" name="txtID" id="txtID"  placeholder="ID">
+                    <input type="hidden" class="form-control" name="txtID" id="txtID" value="<?php echo $txtID; ?>" placeholder="ID">
                 </div>
 
                 <div class = "form-group">
                     <label for="txtNom"> Nom llibre:</label>
-                    <input type="text" class="form-control" name="txtNom" id="txtNom"  placeholder="Nom del llibre">
+                    <input type="text" class="form-control" name="txtNom" id="txtNom"  value="<?php echo $txtNom; ?>" placeholder="Nom del llibre">
                 </div>
 
                 <div class = "form-group">
                     <label for="txtImatge">Imatge: </label>
+                    <?php echo $txtImg; ?>
                     <input type="file" class="form-control" name="txtImatge" id="txtImatge" accept="image/*">
                 </div>
 
@@ -82,12 +114,21 @@ switch ($accion) {
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td>2</td>
-                <td>Apren</td>
-                <td>imatge.jpg</td>
-                <td>Seleccionar | Borrar</td>
-            </tr>
+            <?php foreach($llistaLlibres as $llibre){ ?>
+                <tr>
+                    <td> <?php echo $llibre['id']; ?></td>
+                    <td><?php echo $llibre['nom']; ?></td>
+                    <td><?php echo $llibre['imatge']; ?></td>
+
+                    <td>            
+                        <form method="post"> 
+                            <input type="hidden" name="txtID" id="txtID" value="<?php echo $llibre['id']; ?>"/>
+                            <input type="submit" name="accion" value="Seleccionar" class="btn btn-primary"> 
+                            <input type="submit" name="accion" value="Eliminar" class="btn btn-danger"> 
+                        </form>
+                    </td>
+                </tr>
+            <?php } ?>
         </tbody>
     </table>
     
